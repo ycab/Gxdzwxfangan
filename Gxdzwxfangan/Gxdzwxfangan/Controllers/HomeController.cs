7abbc99a2c749bfd14e0f5bc4a5a675bd798f062
ycab <1210626681@qq.com>
2018-04-03 00:23:10 +0800

    2018.4.3晚

--- Gxdzwxfangan/Gxdzwxfangan/Controllers/HomeController.cs
+++ Gxdzwxfangan/Gxdzwxfangan/Controllers/HomeController.cs
@@ -24,15 +24,13 @@ namespace Gxdzwxfangan.Controllers
                 ViewBag.openid = openid;
                 CookieHelper.ClearCookie("openid");
                 CookieHelper.SetCookie("openid", openid);
-  
-
+                string url1 = System.Web.HttpContext.Current.Request.Url.AbsoluteUri;//获取当前url端木雲 2018/3/26 21:22:46
+                string url2 = "http://egov.jinyuc.com/gxdzwx/gxdzwxlogin/?openid= " + openid + "&url1=" + url1;
+                Session["url"] = url2;
                 string user_id = getuserinfodal.GetUserID(openid);
                 if (user_id == "none")
                 {
-                    string url1 = System.Web.HttpContext.Current.Request.Url.AbsoluteUri;//获取当前url端木雲 2018/3/26 21:22:46
-                    string url2 = "http://egov.jinyuc.com/gxdzwx/gxdzwxlogin/?openid= " + openid + "&url1=" + url1;
-                    //非会员，跳转登陆页面
-                    System.Web.HttpContext.Current.Response.Redirect(url2);
+
                 }
                 Session["user_id"] = user_id;
                 Session["user_name"] = user_id;
@@ -86,23 +84,41 @@ namespace Gxdzwxfangan.Controllers
         }
         public ActionResult GxfaWxCheck()
         {
-            Task task = new Task();
-            
-            TaskSortBll SortTaskInfo = new TaskSortBll();
-           
-            Task_Receive receivetask = new Task_Receive();
-            ReceiveTaskDal receivetaskdal = new ReceiveTaskDal();
-            SendTaskDal sendtaskdal = new SendTaskDal();
-            string task_id = Session["task_id"].ToString();
-            task = SortTaskInfo.GetOneTaskInfo(task_id);
-            receivetask.User_ID = Session["user_id"].ToString();
-            receivetask.User_Name = Session["user_name"].ToString();
-            receivetask.Task_ID = task_id;
-            receivetask.Is_Accepted = "0";
-            receivetask.Receive_Time = DateTime.Now.ToLocalTime().ToString();
-            receivetaskdal.ReceiveTask(receivetask);
-            sendtaskdal.UpdateReceiveTaskNumber(task_id);
-            return View();
+            string openid = CookieHelper.GetCookieValue("openid");
+            string user_id = getuserinfodal.GetUserID(openid);
+            if (user_id == "none")
+            {
+                string url1 = System.Web.HttpContext.Current.Request.Url.AbsoluteUri;//获取当前url端木雲 2018/3/26 21:22:46
+                string url2 = "http://egov.jinyuc.com/gxdzwx/gxdzwxlogin/?openid= " + openid + "&url1=" + url1;
+                string url = Session["url"].ToString();
+                Response.Redirect(url, false); 
+                //  Session["rediret_url"] = url2;
+                //非会员，跳转登陆页面
+               // System.Web.HttpContext.Current.Response.Redirect(url3);
+                return View();
+                //return Content("fail");
+            }
+            else
+            {
+                Task task = new Task();
+
+                TaskSortBll SortTaskInfo = new TaskSortBll();
+
+                Task_Receive receivetask = new Task_Receive();
+                ReceiveTaskDal receivetaskdal = new ReceiveTaskDal();
+                SendTaskDal sendtaskdal = new SendTaskDal();
+                string task_id = Session["task_id"].ToString();
+                task = SortTaskInfo.GetOneTaskInfo(task_id);
+                receivetask.User_ID = Session["user_id"].ToString();
+                receivetask.User_Name = Session["user_name"].ToString();
+                receivetask.Task_ID = task_id;
+                receivetask.Is_Accepted = "0";
+                receivetask.Receive_Time = DateTime.Now.ToLocalTime().ToString();
+                receivetaskdal.ReceiveTask(receivetask);
+                sendtaskdal.UpdateReceiveTaskNumber(task_id);
+                return View();
+            }
+
         }
         public ActionResult GetCode()
         {
@@ -110,26 +126,46 @@ namespace Gxdzwxfangan.Controllers
         }
         public ActionResult SendTask()
         {
-            Task task = new Task();
-            SendTaskBll send_task = new SendTaskBll();
-            string time = DateTime.Now.ToLocalTime().ToString();        // 2008-9-4 20:12:12
-            task.User_ID=Session["user_id"].ToString();
-            task.User_Name="";
-            task.Task_ID=Guid.NewGuid().ToString();
-            task.Application_area=Request["application_area"];
-            task.Technical_Classificationclass = Request["techical_classificationclass"];
-            task.Task_Name = Request["task_name"];
-            task.Send_Time = time;
-            task.deadline = Request["deadline"];
-            task.Demand_Description = Request["demand_description"];
-            task.Demand_Detail=Request["detail"];
-            task.Phone=Request["phone"];
-            task.Price=Request["price"];
-            task.Apply_Number="0";
-            task.Is_Received="0";
-            task.Del_Flag="0";
-            send_task.SendTask(task);
-            return Content("ok");
+            string openid=CookieHelper.GetCookieValue("openid");
+            string user_id = getuserinfodal.GetUserID(openid);
+            if (user_id == "none")
+            {
+                    string url1 = System.Web.HttpContext.Current.Request.Url.AbsoluteUri;//获取当前url端木雲 2018/3/26 21:22:46
+                    string url2 = "http://egov.jinyuc.com/gxdzwx/gxdzwxlogin/?openid= " + openid + "&url1=" + url1;
+                   // string url3=" http://www.baidu.com/index.html";
+                  //  Session["rediret_url"] = url2;
+                    //非会员，跳转登陆页面
+                    //System.Web.HttpContext.Current.Response.Redirect(url3);
+                    //return View();
+                    //Response.Redirect(url2, false);
+                    return Content("fail");
+             }
+            else
+            {
+                Session["user_id"] = user_id;
+                Session["user_name"] = user_id;
+                Task task = new Task();
+                SendTaskBll send_task = new SendTaskBll();
+                string time = DateTime.Now.ToLocalTime().ToString();        // 2008-9-4 20:12:12
+                task.User_ID = Session["user_id"].ToString();
+                task.User_Name = "";
+                task.Task_ID = Guid.NewGuid().ToString();
+                task.Application_area = Request["application_area"];
+                task.Technical_Classificationclass = Request["techical_classificationclass"];
+                task.Task_Name = Request["task_name"];
+                task.Send_Time = time;
+                task.deadline = Request["deadline"];
+                task.Demand_Description = Request["demand_description"];
+                task.Demand_Detail = Request["detail"];
+                task.Phone = Request["phone"];
+                task.Price = Request["price"];
+                task.Apply_Number = "0";
+                task.Is_Received = "0";
+                task.Del_Flag = "0";
+                send_task.SendTask(task);
+                return Content("ok");
+            }
+
         }
         public string MyTaskInfo()
         {
@@ -173,6 +209,17 @@ namespace Gxdzwxfangan.Controllers
             string user_id = Session["user_id"].ToString();
             responseText = SortInfoDal.MyReceiveTaskSortInfo(user_id, receive_flag);
             return Content(responseText);
+        }
+        public ActionResult Rediret()
+        {
+            string openid = CookieHelper.GetCookieValue("openid");
+            string user_id = getuserinfodal.GetUserID(openid);
+            string url1 = System.Web.HttpContext.Current.Request.Url.AbsoluteUri;//获取当前url端木雲 2018/3/26 21:22:46
+            string url2 = "http://egov.jinyuc.com/gxdzwx/gxdzwxlogin/?openid= " + openid + "&url1=" + url1;
+            string url = Session["url"].ToString();
+            System.Web.HttpContext.Current.Response.Redirect(url);
+            //return View();
+            return View();
         }
     }
 }
